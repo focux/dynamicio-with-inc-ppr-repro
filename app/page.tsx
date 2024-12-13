71ddc7e1d80eb8024434a17435d47a23c76df0a2
Leonardo E. Dominguez <focuux@gmail.com>
2024-12-13 09:41:13 -0400

    add repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-/** Add your relevant code here for the issue to reproduce */
+'use cache';
+import { unstable_cacheLife as cacheLife } from "next/cache";
+
+export const experimental_ppr = true;
+
 export default function Home() {
+  cacheLife("seconds");
   return null;
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@
  */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    dynamicIO: true,
+    ppr: "incremental",
+  },
 };
 
 export default nextConfig;
