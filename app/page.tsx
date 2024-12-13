4e82dd7871f90d30995ad56224cd91a420e0efc0
Leonardo E. Dominguez <focuux@gmail.com>
2024-12-13 09:41:50 -0400

    remove cache life

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 'use cache';
-import { unstable_cacheLife as cacheLife } from "next/cache";
 
 export const experimental_ppr = true;
 
 export default function Home() {
-  cacheLife("seconds");
   return null;
 }
